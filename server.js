acbbe184085e7e657dbe92e4754e1106445978d7
kevin ventura <kevcoder@kevins-mbp.myfiosgateway.com>
2023-06-12 17:39:25 -0400

    wip: Delete route added, we can now delete a scientist from our database

--- server.js
+++ server.js
@@ -3,35 +3,43 @@
 //======================================
 const express = require('express');
 const app = express();
-const scientist = require('./models/scientist')
+const scientist = require('./models/scientist');
+const methodOverride = require("method-override");
 const PORT = 3000;
 
 //======================================
 //          MiddleWare
 //======================================
-app.use(express.urlencoded({ extended: false }))
+app.use(express.urlencoded({ extended: false }));
+
+app.use(methodOverride("_method"));
+
 
 //======================================
 //          ROUTES
 //======================================
 //INDEX
 app.get('/exobiology', (req, res) => {
-  res.render('index.ejs', {scientist})
+  res.render('index.ejs', {scientist});
 })
 
 //NEW
 app.get('/exobiology/new', (req, res) => {
-  res.render('new.ejs')
+  res.render('new.ejs');
 })
 
 //CREATE 
 app.post('/exobiology/', (req, res) => {
   console.log('New Scientist:', req.body);
   scientist.push(req.body);
-  
   res.redirect('/exobiology');
 })
 
+//DELETE
+app.delete("/exobiology/:id", (req, res) => {
+  scientist.splice(req.params.id, 1);
+  res.redirect('/exobiology');
+})
 
 //======================================
 //          LISTENERS
@@ -41,4 +49,3 @@ app.listen(PORT, () => {
 })
 
 
-console.log(scientist)
